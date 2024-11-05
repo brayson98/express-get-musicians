daf1bf4b1d0901d1ee5d34752e7bdeb32a565a4e
Bart <brayson@live.com>
2024-11-05 11:52:47 +0000

    added crud routes

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get("/musicians", async (req, res) => {
     try {
@@ -17,8 +19,48 @@ app.get("/musicians", async (req, res) => {
     }
 });
 
+app.get("/musicians/:id", async (req, res) => {
+    try {
+        const musician = await Musician.findByPk(req.params.id)
+        res.json(musician);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to retrieve musician" });
+    }
+});
 
-app.get()
+app.post('/musicians', async (req, res) => {
+    try {
+        const musician = await Musician.create(req.body);
+        res.status(201).send(musician)
+    } catch (err) {
+        res.sendStatus(500);
+        console.error(err);
+      }
+})
+
+app.put('/musicians/:id', async (req, res) => {
+    try {
+        let musician = await Musician.findByPk(req.params.id);
+        if (!musician) return res.status(404);
+        musician = await musician.update(req.body, {where: {id: req.params.id}})
+        res.send(musician)
+    } catch (err) {
+        res.sendStatus(500);
+        console.error(err);
+      }
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    try {
+        let musician = await Musician.findByPk(req.params.id);
+        if (!musician) return res.sendStatus(404);
+        musician = await musician.destroy();
+        res.send(musician)
+    }catch (err) {
+        res.sendStatus(500);
+        console.error(err);
+      }
+})
 
 
 
